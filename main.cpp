85dc58335092d268aaa7635e717d6c967972acfa
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2019-06-11 21:28:06 -0700

    testing dialog clearing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hello world" << endl;
+    char userInput;
+
+    cout << "Hello, would you like to play?" << flush << endl;
+    cin >> userInput;
+    system("clear");
+    cout << "Are you sure?" << endl;
+    cout << userInput << endl;
+    
 }
