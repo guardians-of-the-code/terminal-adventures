d7e3991d17c97566692e907719415c448102eaac
Jay U <36392692+twotonerebel3@users.noreply.github.com>
2019-06-13 21:35:56 -0700

    Update main.cpp
    Added comments

--- main.cpp
+++ main.cpp
@@ -3,32 +3,32 @@
 const int height=20;
 const int width=100;
 using namespace std;
-class terminal {
+class terminal {// declared class
 public:
-	void draw()
+	void draw()// function for class
 	{
-		for (int x = 0; x < height; x++)
+		for (int x = 0; x < height; x++)// Says whioh row it is
 		{
-			for (int y = 0; y < width; y++)
+			for (int y = 0; y < width; y++)// says which element of the row it is
 			{
-				if (x == 0 || x == height - 1)
+				if (x == 0 || x == height - 1)// iterates through the whole row if it is at 0(top) or height-1(bottom)
 				{
 					cout << "#";
 				}
 				else
 				{
-					if (y == 0 || y == width - 1)
+					if (y == 0 || y == width - 1)// Acts as a border for middle of screen.Adding #only if at first element 0 or  at end element width-1
 					{
 						cout << "#";
 					}
-					else
+					else// If neither of above is true, it fills it in with whitespace, giving user a box.
 					{
 						cout << " ";
 					}
 				}
 
 			}
-			cout << endl;
+			cout << endl;// ends line after row is complete and starts next row.
 		}
 	}
 
@@ -36,8 +36,8 @@ public:
 
 int main() 
 {
-	terminal map1;
+	terminal map1; // declare class variable
 	cout << "Hello world" << endl;
-	map1.draw();
+	map1.draw();// calling variable function
 
 }
