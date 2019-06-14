3197c932956bacc7a572cba0663ad366cccda68f
Jay <jupadhyay1@yahoo.com>
2019-06-13 21:06:01 -0700

    Created a adjustable box. Also a class.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 #include <string>
-
+const int height=20;
+const int width=100;
 using namespace std;
+class terminal {
+public:
+	void draw()
+	{
+		for (int x = 0; x < height; x++)
+		{
+			for (int y = 0; y < width; y++)
+			{
+				if (x == 0 || x == height - 1)
+				{
+					cout << "#";
+				}
+				else
+				{
+					if (y == 0 || y == width - 1)
+					{
+						cout << "#";
+					}
+					else
+					{
+						cout << " ";
+					}
+				}
 
-int main() {
+			}
+			cout << endl;
+		}
+	}
+
+};
+
+int main() 
+{
+	terminal map1;
     cout << "Hello world" << endl;
+	map1.draw();
+
 }
